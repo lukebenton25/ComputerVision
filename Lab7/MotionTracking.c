aaa18204176a0593e53ff824c410143cdffe7299
lukebenton25 <47285799+lukebenton25@users.noreply.github.com>
2019-11-25 22:00:36 -0500

    fucking done

--- Lab7/MotionTracking.c
+++ Lab7/MotionTracking.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdbool.h>
 #include "MotionTracking.h"
 
 #define SQR(x) ((x)*(x))
@@ -9,28 +10,20 @@
 int main()
 {
 	float **Table;
-	float *Xbuff1;
-	float *Xbuff2;
-	float *Ybuff1;
-	float *Ybuff2;
-	float *Zbuff1;
-	float *Zbuff2;
-	float *Pbuff1;
-	float *Pbuff2;
-	float *Rbuff1;
-	float *Rbuff2;
-	float *Wbuff1;
-	float *Wbuff2;
 	FILE  *fpt;
-	int   i;
+	FILE  *fpt2;
+	FILE  *Output;
+	bool  moving;
+	int   i, j;
 	char  c;
-	int   data_size;
-	float accXAvg  = 0.0;
-	float accYAvg  = 0.0;
-	float accZAvg  = 0.0;
-	float pitchAvg = 0.0;
-	float rollAvg  = 0.0;
-	float yawAvg   = 0.0;
+	float var;
+	float dist_arr[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	float var_arr[7] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	float start_time = 0.0;
+	float end_time = 0.0;
+	float startIndex = 0;
+	float endIndex = 0;
+
 	
 	// Read beginning contour points
 	if ((fpt=fopen("acc_gyro.txt", "rb")) == NULL)
@@ -47,19 +40,19 @@ int main()
 			i++;
 	}
 	
-	data_size = i;
+	DATA_SIZE = i;
 	
 	rewind(fpt);
 	
 	Table  = (float **)malloc(7*sizeof(float *));
 	for (i = 0; i < 7; i++)
 	{
-		Table[i] = (float *)malloc(data_size*sizeof(float));
+		Table[i] = (float *)malloc(DATA_SIZE*sizeof(float));
 	}
 	
 	i = 0;
 	
-	while(!feof(fpt) && !(i == data_size))
+	while(!feof(fpt) && !(i == DATA_SIZE))
 	{
 		if (i == 0)
 		{
@@ -68,36 +61,151 @@ int main()
 		else
 		{
 			fscanf(fpt, "%f %f %f %f %f %f %f \n", &Table[TIME][i-1], &Table[ACCX][i-1], &Table[ACCY][i-1], &Table[ACCZ][i-1], &Table[PITCH][i-1], &Table[ROLL][i-1], &Table[YAW][i-1]);
-			accXAvg  += Table[ACCX][i-1];
-			accYAvg  += Table[ACCY][i-1];
-			accZAvg  += Table[ACCZ][i-1];
-			pitchAvg += Table[PITCH][i-1];
-			rollAvg  += Table[ROLL][i-1];
-			yawAvg   += Table[YAW][i-1];
 		}
 		i++;
 	}
 	
-	accXAvg  = (accXAvg / data_size)/4;
-	accYAvg  = (accYAvg / data_size)/4;
-	accZAvg  = (accZAvg / data_size)*1.2;
-	pitchAvg = pitchAvg / data_size;
-	rollAvg  = rollAvg / data_size;
-	yawAvg   = yawAvg / data_size;
-	
-	printf("ACCX Average: %f\n", accXAvg);
-	printf("ACCY Average: %f\n", accYAvg);
-	printf("ACCZ Average: %f\n", accZAvg);
-	printf("Pitch Avergae: %f\n", pitchAvg);
-	printf("Roll Average: %f\n", rollAvg);
-	printf("Yaw Average: %f\n", yawAvg);
-	
-	
-	/* for (i = 0; i < data_size; i++)
+	for (i = 0; i < DATA_SIZE; i++)
 	{
-		
-	} */
+		Table[ACCZ][i] += 1;
+	}
 	
+	fpt2 = fopen("variance.csv", "w");
+	fprintf(fpt2, "TIME,ACCX,ACCY,ACCZ,PITCH,ROLL,YAW\n");
+	
+	for (i = 0; i < DATA_SIZE; i++)
+	{
+		fprintf(fpt2, "%f,", Table[TIME][i]);
+		for (j = ACCX; j <= YAW; j++)
+		{
+			var = variance(Table[j], i);
+			fprintf(fpt2, "%f,", var);
+		}
+		fprintf(fpt2, "\n");
+	}
+	fclose(fpt2);
+	
+	Output = fopen("Results.txt", "w");
+	fprintf(Output, "RESULTS:\n");
+	
+	for (i = 0; i < DATA_SIZE; i++)
+	{
+		for (j = ACCX; j <= YAW; j++)
+		{
+			var_arr[j] = variance(Table[j], i);
+		}
+		moving = isMoving(var_arr);
+		if (moving && start_time == 0.0)
+		{
+			start_time = Table[TIME][i];
+			startIndex = i;
+		}
+		else if (!moving && start_time != 0.0 && end_time == 0.0)
+		{
+			moving = false;
+			end_time = Table[TIME][i];
+			endIndex = i;
+		}
+		if (start_time != 0.0 && end_time != 0.0)
+		{
+			dist_arr[ACCX] = integrate_acc(Table[ACCX], startIndex, endIndex);
+			dist_arr[ACCY] = integrate_acc(Table[ACCY], startIndex, endIndex);
+			dist_arr[ACCZ] = integrate_acc(Table[ACCZ], startIndex, endIndex);
+			dist_arr[PITCH] = integrate(Table[PITCH], startIndex, endIndex);
+			dist_arr[ROLL] = integrate(Table[ROLL], startIndex, endIndex);
+			dist_arr[YAW] = integrate(Table[YAW], startIndex, endIndex);
+			
+			PrintData(Output, dist_arr, start_time, end_time, startIndex, endIndex);
+			start_time = 0.0;
+			end_time = 0.0;
+		}
+		
+	}
+	fclose(Output);
 	return(0);
 }
+
+void PrintData(FILE *fp, float distance[7], float start_time, float end_time, int start_index, int end_index)
+{
+	fprintf(fp, "########################################################\n");
+	fprintf(fp, "X Movement: %f [m]\nY Movement: %f [m]\nZ Movement: %f [m]\n", distance[ACCX], distance[ACCY], distance[ACCZ]);
+	fprintf(fp, "Pitch Movement: %f [rad]\nRoll Movement: %f [rad]\nYaw Movement: %f [rad]\n", distance[PITCH], distance[ROLL], distance[YAW]);
+	fprintf(fp, "Start Time: %0.2f\t\tEnd Time: %0.2f\n", start_time, end_time);
+	fprintf(fp, "Start Index: %d\t\tEnd Index: %d\n", start_index, end_index);
+	fprintf(fp, "\n\n");
 	
+	return;
+}
+
+float integrate_acc(float *input, int start, int end)
+{
+	int i;
+	float prev_velocity = 0.0;
+	float velocity = 0.0;
+	float distance = 0.0;
+	
+	for (i = start; i <= end; i++)
+	{
+		prev_velocity = velocity;
+		velocity += input[i] * GRAVITY * SAMPLE_TIME;
+		distance += ((velocity + prev_velocity) / 2) * SAMPLE_TIME;
+	}
+	
+	return (distance);
+}
+
+float integrate(float *input, int start, int end)
+{
+	float ret_val = 0.0;
+	int   i;
+	for (i = start; i <= end; i++)
+	{
+		ret_val += input[i] * SAMPLE_TIME;
+	}
+	
+	return (ret_val);
+}
+
+float variance (float *input, int curr_index)
+{
+	int   i;
+	int   local_var_window;
+	float mean = 0;
+	float var;
+	if (curr_index + VAR_WINDOW <= DATA_SIZE)
+	{
+		local_var_window = curr_index + VAR_WINDOW;
+	}
+	else
+	{
+		local_var_window = DATA_SIZE;
+	}
+	for (i = curr_index; i < local_var_window; i++)
+	{
+		mean += input[i];
+	}
+	mean = mean / (VAR_WINDOW  + 1);
+	
+	for (i = curr_index; i < local_var_window; i++)
+	{
+		var += SQR(input[i] - mean);
+	}
+	var = var / (VAR_WINDOW + 1);
+	
+	return var;
+}
+
+bool isMoving(float var_arr[7])
+{
+	bool ret_val = false;
+	if (var_arr[ACCX] > ACC_THRESH || var_arr[ACCY] > ACC_THRESH || var_arr[ACCZ] > ACC_THRESH)
+	{
+		ret_val = true;
+	}
+	if (var_arr[PITCH] > PITCH_THRESH || var_arr[ROLL] > ROLL_THRESH || var_arr[YAW] > YAW_THRESH)
+	{
+		ret_val = true;
+	}
+	
+	return (ret_val);
+}
--- Lab7/MotionTracking.h
+++ Lab7/MotionTracking.h
@@ -7,3 +7,19 @@ enum {
 	ROLL,
 	YAW
 };
+
+int DATA_SIZE;
+
+#define VAR_WINDOW 10
+#define ACC_THRESH 0.0001
+#define PITCH_THRESH 0.005
+#define ROLL_THRESH 0.05
+#define YAW_THRESH 0.005
+#define SAMPLE_TIME 0.05
+#define GRAVITY 9.81
+
+float variance (float *input, int curr_index);
+void PrintData(FILE *fp, float distance[7], float start_time, float end_time, int start_index, int end_index);
+float integrate_acc(float *input, int start, int end);
+float integrate(float *input, int start, int end);
+bool isMoving(float var_arr[7]);
